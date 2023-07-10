720c2153dee8037717e1f9c4f9fa1e6bbff88982
Drew Hackworth <Drew.Hackworth@itpolaris.com>
2023-07-10 14:07:02 -0400

    restored route.tsx file

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,30 +1,14 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 import NextAuth from "next-auth";
-import {
-    getServerSession,
-    type NextAuthOptions,
-    type DefaultSession,
-  } from "next-auth";
+
 import EmailProvider from "next-auth/providers/email";
 import DiscordProvider from "next-auth/providers/discord";
 import AzureADProvider from "next-auth/providers/azure-ad";
-import { type GetServerSidePropsContext } from "next";
 
 const prisma = new PrismaClient();
 
-
-
-const authOptions: NextAuthOptions = {
-    callbacks: {
-      session: ({ session, user }) => ({
-        ...session,
-        user: {
-          ...session.user,
-          id: user.id,
-        },
-      }),
-    },
+const handler = NextAuth({
     providers: [
       
         DiscordProvider({
@@ -50,32 +34,6 @@ const authOptions: NextAuthOptions = {
         }),
     ],
     adapter: PrismaAdapter(prisma),
-  };
+  });
 
-  declare module "next-auth" {
-    interface Session extends DefaultSession {
-      user: {
-        id: string;
-        // ...other properties
-        // role: UserRole;
-      } & DefaultSession["user"];
-    }
-  
-    // interface User {
-    //   // ...other properties
-    //   // role: UserRole;
-    // }
-  }
-
-/**
- * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
- *
- * @see https://next-auth.js.org/configuration/nextjs
- */
-export const getServerAuthSession = (ctx: {
-    req: GetServerSidePropsContext["req"];
-    res: GetServerSidePropsContext["res"];
-  }) => {
-    return getServerSession(ctx.req, ctx.res, authOptions);
-  };
-
+  export { handler as GET, handler as POST };
