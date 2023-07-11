1a7c46c75cb3ccc39ea0a0e599598103598717f2
Drew Hackworth <Drew.Hackworth@itpolaris.com>
2023-07-11 15:42:13 -0400

    Update route.tsx

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -31,6 +31,15 @@ const handler = NextAuth({
             id: user.id,
         },
         }),
+        async redirect({ url, baseUrl }) {
+            // Allows relative callback URLs
+            if (url.startsWith("/")) return `${baseUrl}${url}`
+            // Allows callback URLs on the same origin
+            else if (new URL(url).origin === baseUrl) return url
+            return baseUrl
+          }
+        
+        
   },    
     providers: [
       
